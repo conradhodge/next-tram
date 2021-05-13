96eed504f7b200b070d92410dd53d37f91f2bd3f
Conrad Hodge <me@conski.co.uk>
2021-05-13 22:08:12 +0100

    Add prettier
    - Added Prettier to format the code
    - Added prettier to lint-staged to run as in the pre-commit hook
    - Added pretter to GitHub action
    
    https://prettier.io/docs/en/index.html

--- infrastructure/bin/next-tram.ts
+++ infrastructure/bin/next-tram.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { NextTramStack } from '../lib/next-tram-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { NextTramStack } from "../lib/next-tram-stack";
 
 const app = new cdk.App();
-new NextTramStack(app, 'next-tram-stack', {
-  description: "Lambda to provide Alexa skill that uses the Traveline API to fetch the time of the next tram"
+new NextTramStack(app, "next-tram-stack", {
+  description:
+    "Lambda to provide Alexa skill that uses the Traveline API to fetch the time of the next tram",
 });
--- infrastructure/test/next-next-tram.test.ts
+++ infrastructure/test/next-next-tram.test.ts
@@ -1,7 +1,7 @@
 import { haveResource, expect as exp, haveType } from "@aws-cdk/assert";
 import "@aws-cdk/assert/jest";
 import { App } from "@aws-cdk/core";
-import { NextTramStack } from "../lib/next-tram-stack"
+import { NextTramStack } from "../lib/next-tram-stack";
 
 /**
  * This is an example of a fine-grained test. This will check a single
@@ -13,7 +13,9 @@ import { NextTramStack } from "../lib/next-tram-stack"
 test("Stack is created with lambda", () => {
   const context = require("./cdk.json");
   const app = new App(context);
-  const stack = new NextTramStack(app, "MyTestStack", { env: { account: "999999999999", region: "eu-west-2" } });
+  const stack = new NextTramStack(app, "MyTestStack", {
+    env: { account: "999999999999", region: "eu-west-2" },
+  });
   exp(stack).to(
     haveResource("AWS::Lambda::Function", {
       FunctionName: "get-next-tram-lambda",
@@ -25,10 +27,10 @@ test("Stack is created with lambda", () => {
           TRAVELINE_API_USERNAME: "api-username",
           TRAVELINE_API_PASSWORD: "api-password",
           NAPTAN_CODE: "111222333",
-        }
+        },
       },
       MemorySize: 128,
-      Timeout: 10
+      Timeout: 10,
     })
   );
 });
